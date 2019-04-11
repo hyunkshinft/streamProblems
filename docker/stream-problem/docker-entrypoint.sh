1c3ea03cabf1caad6b8b6a8fee9cc2bc075b1e68
jerry and evanzanten <jerwis0731@gmail.com>
2019-04-11 15:26:14 +0000

    Use new 42.2.6 pgjdbc library

--- docker/stream-problem/docker-entrypoint.sh
+++ docker/stream-problem/docker-entrypoint.sh
@@ -20,6 +20,13 @@ wait_for_postgres
 psql -h postgres -U $POSTGRES_USER $POSTGRES_DB -c "select pg_drop_replication_slot('test_slot');" || true
 psql -h postgres -U $POSTGRES_USER $POSTGRES_DB -c "SELECT * FROM pg_create_logical_replication_slot('test_slot', 'test_decoding');"
 
+mvn install:install-file -Dfile=/streamProblems/postgresql-42.2.6-SNAPSHOT.jar \
+  -DgroupId=org.postgresql \
+  -DartifactId=postgresql \
+  -Dversion=42.2.6 \
+  -Dpackaging=jar \
+  -DgeneratePom=true
+
 cd /streamProblems && mvn clean && mvn package
 
 java -cp /streamProblems/docker/stream-problem/streamProblems-1.0.jar streamProblem
